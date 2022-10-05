a473622ad145391d21bc7064a4ed029176d45474
Timibreez <ffftimi@gmail.com>
2022-10-05 15:04:21 +0100

    added the flow to GetStudentById

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AccesStudent.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace AccesStudent.Controllers
 {
@@ -7,5 +10,36 @@ namespace AccesStudent.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetAllStudents")]
+
+        public Response GetAllStudents()
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ASConnection").ToString());
+            Response response = new Response();
+            DTO dto = new DTO();
+            response = dto.GetAllStudents(connection);
+            return response;
+        }
+
+        [HttpGet]
+        [Route("GetStudentById/{id}")]
+
+        public Response GetStudentById(int id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("ASConnection").ToString());
+            Response response = new Response();
+            DTO dto = new DTO();
+            response = dto.GetStudentById(connection, id);
+            return response;
+        }
+
     }
 }
--- Models/DTO.cs
+++ Models/DTO.cs
@@ -11,7 +11,7 @@ namespace AccesStudent.Models
         {
             Response response = new Response();
             SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM aStudent", connection);
-            DataTable dt = new DataTable();
+            DataTable dt = new DataTable(); 
             List<Student> lstStudents = new List<Student>();
             adapter.Fill(dt);
 
@@ -34,6 +34,42 @@ namespace AccesStudent.Models
                 response.StatusCode = 200;
                 response.StatusMessage = "Student Details available";
                 response.listStudents = lstStudents;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "No data found";
+                response.listStudents = null;
+            }
+
+            return response;
+        }
+
+        public Response GetStudentById(SqlConnection connection, int id)
+        {
+            Response response = new Response();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM aStudent WHERE ID = '" + id + "' AND IsActive = 1", connection);
+            DataTable dt = new DataTable();
+            Student Students = new Student();
+            adapter.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                Student student = new Student();
+                student.Id = Convert.ToInt32(dt.Rows[0]["Id"]);
+                student.Name = Convert.ToString(dt.Rows[0]["Name"]);
+                student.Course = Convert.ToString(dt.Rows[0]["Course"]);
+                student.IsActive = Convert.ToInt32(dt.Rows[0]["IsActive"]);
+
+                response.StatusCode = 200;
+                response.StatusMessage = "Student Detail available";
+                response.Student = student;
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "No data found";
+                response.Student = null;
             }
 
             return response;
