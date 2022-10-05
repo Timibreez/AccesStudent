df1f013d8a9b179cead28b31fb82efeda86feecd
Timibreez <ffftimi@gmail.com>
2022-10-05 13:05:24 +0100

    added the get all students method to DTO- data transfer object class, and connected to the database in the student controller using the connection string.

--- Models/DTO.cs
+++ Models/DTO.cs
@@ -1,12 +1,40 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AccesStudent.Models
 {
     public class DTO
     {
-        public Response GetAllStudents(SqlConnection)
+        public Response GetAllStudents(SqlConnection connection)
         {
             Response response = new Response();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM aStudent", connection);
+            DataTable dt = new DataTable();
+            List<Student> lstStudents = new List<Student>();
+            adapter.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(dt.Rows[i]["Id"]);
+                    student.Name = Convert.ToString(dt.Rows[i]["Name"]);
+                    student.Course = Convert.ToString(dt.Rows[i]["Course"]);
+                    student.IsActive = Convert.ToInt32(dt.Rows[i]["IsActive"]);
+
+                    lstStudents.Add(student);
+                }
+            }
+
+            if (lstStudents.Count > 0)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "Student Details available";
+                response.listStudents = lstStudents;
+            }
 
             return response;
         }
